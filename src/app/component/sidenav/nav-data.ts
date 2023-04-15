751cf1390ad7846908e429fb741a91d4494c1a30
Miguel Flores <miguelrhcp14@gmail.com>
2023-04-15 16:10:36 -0500

    Se añadieron componentes visibles en el sidenav

--- src/app/component/sidenav/nav-data.ts
+++ src/app/component/sidenav/nav-data.ts
@@ -1,17 +1,37 @@
 export const navbarData = [
-    {
-        routeLink: 'dashboard',
-        icon: 'fal fa-home',
-        label: 'Panel'
-    },
-    {
-        routeLink: 'clients',
-        icon: 'fal fa-users',
-        label: 'Clientes'
-    },
-    {
-      routeLink: 'gym',
-      icon: 'fal fa-sitemap',
-      label: 'Gimnasios'
+  {
+    routeLink: 'dashboard',
+    icon: 'fal fa-home',
+    label: 'Panel',
+  },
+  {
+    routeLink: 'clients',
+    icon: 'fal fa-users',
+    label: 'Clientes',
+  },
+  {
+    routeLink: 'gym',
+    icon: 'fal fa-sitemap',
+    label: 'Gimnasios',
+  },
+  {
+    routeLink: 'exercise',
+    icon: 'fal fa-dumbbell',
+    label: 'Ejercicios',
+  },
+  {
+    routeLink: 'routines',
+    icon: 'fal fa-paste',
+    label: 'Rutinas',
+  },
+  {
+    routeLink: 'nutritional',
+    icon: 'fal fa-user-md',
+    label: 'Nutricionista',
+  },
+  {
+    routeLink: 'foods',
+    icon: 'fal fa-salad',
+    label: 'Alimentos',
   },
 ];
