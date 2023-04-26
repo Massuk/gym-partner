b70a489e154d9182aa64fd2c51f830b80389d91c
MelWatson <u20201b276@upc.edu.pe>
2023-04-25 19:08:16 -0500

    insercion del componente exercise-delete

--- src/app/component/entities/exercise/exercise-list/exercise-list.component.ts
+++ src/app/component/entities/exercise/exercise-list/exercise-list.component.ts
@@ -1,8 +1,11 @@
-
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ExerciseService } from 'src/app/service/exercise.service';
 import { exercise } from 'src/app/model/exercise';
 import {MatTableDataSource} from '@angular/material/table'
+import { MatDialog } from '@angular/material/dialog'
+import { ConfirmationDialogComponent } from '../../../dashboard/confirmation-dialog/confirmation-dialog.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatPaginator } from '@angular/material/paginator';
 @Component({
   selector: 'app-exercise-list',
   templateUrl: './exercise-list.component.html',
@@ -13,7 +16,9 @@ export class ExerciseListComponent implements OnInit{
   lista:exercise[]=[];
   dataSource:MatTableDataSource<exercise> = new MatTableDataSource();
   displayedColumns:string[] = ['id','nameExercise','series','kilograms','repetitions','Editar']
-  constructor(private eS:ExerciseService){}
+
+  constructor(private eS:ExerciseService, private dialog: MatDialog,
+    private snackBar: MatSnackBar){}
   ngOnInit(): void {
     this.eS.list().subscribe(data=>{this.dataSource = new MatTableDataSource(data)})
     this.eS.getList().subscribe(data=>(this.dataSource=new MatTableDataSource(data)))
@@ -21,5 +26,37 @@ export class ExerciseListComponent implements OnInit{
   filtrar(exercise:any){
     this.dataSource.filter =exercise.target.value.trim();
   }
+
+    @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  openConfirmationDialog(id: number): void {
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      width: '450px',
+      data: { message: '¿Quieres eliminar el ejercicio?' },
+    });
+
+    const snack = this.snackBar;
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.eS.delete(id).subscribe(() => {
+          this.eS.list().subscribe((data) => {
+            this.dataSource = new MatTableDataSource(data);
+            this.dataSource.paginator = this.paginator;
+          });
+        });
+        snack.dismiss();
+        this.snackBar.open('Se ha eliminado correctamente', 'Cerrar', {
+          duration: 3000,
+        });
+      } else {
+        snack.dismiss();
+      }
+    });
+  }
+
+
+
+
 
 }
