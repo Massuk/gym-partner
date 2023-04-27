f49ee04ba6cbe85f7764594ac1322522ee998565
Miguel Flores <miguelrhcp14@gmail.com>
2023-04-26 22:17:43 -0500

    Inserción de landing page

--- src/app/component/landing-page/index/index.component.ts
+++ src/app/component/landing-page/index/index.component.ts
@@ -14,26 +14,15 @@ export class IndexComponent implements OnInit {
   }
   ngOnInit(): void {
 
-    /*------------------
-      Preloader
-    --------------------*/
-    console.log('Preloader started');
-    $('.loader').fadeOut();
-    $('#preloder').delay(400).fadeOut('slow', function() {
-      console.log('Preloader finished');
-    });
-
     (function ($) {
       /*------------------
         Navigation
+      --------------------*/
 
-        (<any>$('.main-menu')).slicknav({
+      (<any>$('.main-menu')).slicknav({
         appendTo: '.header-section',
         allowParentLinks: true,
       });
-
-      --------------------*/
-
       /*------------------
         Background Set
       --------------------*/
@@ -55,43 +44,6 @@ export class IndexComponent implements OnInit {
         e.preventDefault();
       });
 
-      /*------------------
-		      Circle progress
-	      --------------------*/
-      $('.circle-progress').each(function () {
-        var cpvalue = $(this).data('cpvalue');
-        var cpcolor = $(this).data('cpcolor');
-        var cptitle = $(this).data('cptitle');
-        var cpid = $(this).data('cpid');
-
-        $(this).append(
-          '<div class="' +
-            cpid +
-            '"></div><div class="progress-info"><h2>' +
-            cpvalue +
-            '%</h2><p>' +
-            cptitle +
-            '</p></div>'
-        );
-      });
     })(jQuery);
-
-
-    /*------------------
-      Page Loaded Handler
-    --------------------*/
-    $(document).ready(function() {
-      console.log('Page loaded completely');
-    });
-
-    /*------------------
-      Error Handler for Window Load
-    --------------------*/
-    $(window).on('load', function () {
-      if (document.readyState !== "complete") {
-        console.error("Error: Page did not fully load.");
-        // add your error message here
-      }
-    });
   }
 }
