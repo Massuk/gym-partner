3eb883bc563f11f1706952c9ff373d14ceb61963
Miguel Flores <miguelrhcp14@gmail.com>
2023-04-27 20:41:36 -0500

    Corrección de pequeños errores

--- src/app/model/gym.ts
+++ src/app/model/gym.ts
@@ -1,7 +1,7 @@
-export class Gym{
-  id:number=0
-  nameGym:string=""
-  codeGym:string=""
-  rucGym:number=0
-  rsGym:string=""
+export class Gym {
+  id: number = 0;
+  nameGym: string = '';
+  codeGym: string = '';
+  rucGym: number = 0;
+  rsGym: string = '';
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment={
-  production:false,
+  production:true,
   base:"http://localhost:5000"
 }
