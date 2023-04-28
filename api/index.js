06e0fccb48f0dbd4b7cdf60bde34632bb7eb2c03
Miguel Flores <miguelrhcp14@gmail.com>
2023-04-27 21:32:01 -0500

    Json Server en Vercel

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
-const data = require('./data');
+const express = require('express')
+const cors = require('cors') // Agrega esta línea
 
-module.exports = (req, res) => {
-  res.json(data);
-};
+const data = require('./data')
+
+const server = express()
+
+server.use(cors()) // Agrega esta línea
+
+server.get('/gyms', (req, res) => {
+  res.json(data.gyms)
+})
+
+server.listen(3000, () => {
+  console.log('Server is listening on port 3000')
+})
