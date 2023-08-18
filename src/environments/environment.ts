fede0c6d0d7189057b56e5139d8aa83cce8cd43f
Massuk <miguelrhcp14@gmail.com>
2023-08-18 18:11:51 -0500

    Corrección de puerto para conexión a backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment={
   production:true,
-  base:"http://localhost:8080"
-  //base:"https://backend-gym-partner.onrender.com"
+  base:"https://backend-gym-partner.onrender.com"
 }
